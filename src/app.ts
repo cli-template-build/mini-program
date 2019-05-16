80b749dc04e7477479e8a4ef964c8db6d9c2ca8d
yanlele <331393627@qq.com>
2019-05-16 23:27:53 +0800

    [+]

--- src/app.ts
+++ src/app.ts
@@ -3,5 +3,6 @@ App({
   onLaunch(options?: App.ILaunchShowOption): void {
    console.log(`<${'='.repeat(100)}>`);
    console.log('options', options.query);
+   console.log(`<${'='.repeat(100)}>`);
   }
 });
