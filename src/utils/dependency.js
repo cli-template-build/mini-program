1e85898e5420e0cf33efb5f7dfc834df8fef1367
yanlele <331393627@qq.com>
2019-05-25 12:10:37 +0800

    Revert "[+]modify utils"
    This reverts commit 9683cc0d65820f27213f600d08bb2460eacabadc.

--- src/utils/dependency.js
+++ src/utils/dependency.js
@@ -1,1 +1,6 @@
 import 'regenerator-runtime/runtime';
+
+console.log('\u2665');
+console.log(`<${'='.repeat(100)}>`);
+console.log('yanel');
+console.log(`<${'='.repeat(100)}>`);
--- src/utils/util.js
+++
@@ -1,4 +0,0 @@
-export const promise = handler => () =>
-  new Promise((resolve, reject) => {
-    handler({success: resolve, fail: reject});
-  });
